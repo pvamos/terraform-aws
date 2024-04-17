8d19215cdc44fbb734507bf56af3c575c63b31b8
Péter Vámos <pvamos@gmail.com>
2024-04-17 20:01:53 +0200

    excercise-2 AWS region removed from tfvars, added as module param

--- excercise-2/example_usage.tf
+++ excercise-2/example_usage.tf
@@ -1,12 +1,9 @@
 module "s3_backup" {
-  aws_region               = var.main_aws_region
+  source                   = "./modules/s3_backup"
+  aws_region               = "us-east-1"
   aws_access_key           = var.main_aws_access_key
   aws_secret_key           = var.main_aws_secret_key
-  source                   = "./modules/s3_backup"
   bucket_name              = "company-backup-bucket"
   retention_days           = 180
   backup_uploader_role_arn = "arn:aws:iam::123456789012:role/backup_uploader"
 }
-
-
-
--- excercise-2/variables.tf
+++ excercise-2/variables.tf
@@ -1,9 +1,4 @@
 
-variable "main_aws_region" {
-  type = string
-  description = "AWS region parameter"
-}
-
 variable "main_aws_access_key" {
   type = string
   description = "AWS provider access_key parameter"
